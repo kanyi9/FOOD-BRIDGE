21f4b3f309324c5c7d701379932b7dd53d8a2aae
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-08-14 19:36:11 +0300

    dealing with payment form

--- front-end/src/components/DonationPage.jsx
+++ front-end/src/components/DonationPage.jsx
@@ -1,100 +1,133 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import donationImage from '../images/DonationPage.jpg'; 
+import { useNavigate } from 'react-router-dom';
+import './DonationPage.css'; 
 
 const DonationPage = () => {
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const navigate = useNavigate(); 
+
   const handleDonate = () => {
-    alert('You have successfully donated!');
+    alert('Payment processed successfully!');
+    navigate('/Home'); 
+  };
+
+  const handlePaymentMethodChange = (event) => {
+    setPaymentMethod(event.target.value);
   };
 
   return (
-    <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12 flex justify-center items-center h-screen">
-      <div className="bg-yellow-800 shadow-md rounded-md p-6 w-full md:w-1/2 lg:w-1/3">
-        <h1 className="text-3xl font-bold mb-4 text-center text-white">Make a Donation</h1>
-        <p className="text-lg mb-6 text-center text-white">Your donation will help us make a difference in the lives of those in need.</p>
-        <form>
-          <div className="flex flex-wrap mb-4 justify-center">
-            <label className="block w-full md:w-1/2 mb-2 md:mb-0 pr-4">
-              <span className="text-lg text-white">Amount:</span>
-              <input
-                type="number"
-                className="w-full p-2 pl-10 text-lg border border-gray-300 rounded-md"
-                placeholder="Enter amount"
-              />
-            </label>
-            <label className="block w-full md:w-1/2 mb-2 md:mb-0 pl-4">
-              <span className="text-lg text-white">Frequency:</span>
-              <select className="w-full p-2 pl-10 text-lg border border-gray-300 rounded-md">
-                <option value="one-time">One-time</option>
-                <option value="monthly">Monthly</option>
-                <option value="yearly">Yearly</option>
-              </select>
-            </label>
+    <div className="flex min-h-screen">
+      <div className="w-full md:w-1/2 flex items-center justify-center p-6 relative">
+        <img src={donationImage} alt="Donation" className="absolute inset-0 w-full h-full object-cover" />
+        <div className="text-center relative z-10">
+
+        </div>
+      </div>
+
+
+      <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-6">
+        <div className="payment-form-container">
+          <div className="payment-form-header">
+            <h2 className="text-4xl font-bold mb-4">GIVE YOUR DONATION</h2>
+            <p className="text-lg mb-6">Your generosity helps us create a positive impact in the lives of those in need.</p>
           </div>
-          <div className="flex flex-wrap mb-4 justify-center text-white">
-            <label className="block w-full mb-2">
-              <span className="text-lg">Payment Method:</span>
-              <div className="flex justify-center">
+          <form className="p-6 flex flex-col space-y-4">
+            <div className="flex flex-col space-y-4 mb-6">
+              <label className="flex flex-col">
+                <span className="text-lg font-medium mb-2">Amount</span>
                 <input
-                  type="radio"
-                  name="payment-method"
-                  value="credit-card"
-                  className="mr-2"
+                  type="number"
+                  className="input-field"
+                  placeholder="Enter amount"
                 />
-                <span className="text-lg">Credit Card</span>
-                <input
-                  type="radio"
-                  name="payment-method"
-                  value="paypal"
-                  className="mr-2 ml-4"
-                />
-                <span className="text-lg">PayPal</span>
-                <input
-                  type="radio"
-                  name="payment-method"
-                  value="mpesa"
-                  className="mr-2 ml-4"
-                />
-                <span className="text-lg">M-Pesa</span>
-              </div>
-            </label>
-          </div>
-          <div className="flex flex-wrap mb-4 justify-center text-white">
-            <label className="block w-full mb-2">
-              <span className="text-lg">Card Information:</span>
-              <input
-                type="text"
-                className="w-full p-2 pl-10 text-lg border border-gray-300 rounded-md"
-                placeholder="Card number"
-              />
-            </label>
-          </div>
-          <div className="flex flex-wrap mb-4 justify-center text-white">
-            <label className="block w-full mb-2">
-              <span className="text-lg">Expiration Date:</span>
-              <input
-                type="text"
-                className="w-full p-2 pl-10 text-lg border border-gray-300 rounded-md"
-                placeholder="MM/YY"
-              />
-            </label>
-          </div>
-          <div className="flex flex-wrap mb-4 justify-center text-white">
-            <label className="block w-full mb-2">
-              <span className="text-lg">M-Pesa Number:</span>
-              <input
-                type="text"
-                className="w-full p-2 pl-10 text-lg border border-gray-300 rounded-md"
-                placeholder="07xxxxxxxx"
-              />
-            </label>
-          </div>
-          <button
-            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md w-full"
-            onClick={handleDonate}
-          >
-            Donate Now
-          </button>
-        </form>
+              </label>
+              <label className="flex flex-col">
+                <span className="text-lg font-medium mb-2">Frequency</span>
+                <select className="input-field">
+                  <option value="one-time">One-time</option>
+                  <option value="monthly">Monthly</option>
+                  <option value="yearly">Yearly</option>
+                </select>
+              </label>
+              <fieldset className="flex flex-col">
+                <legend className="text-lg font-medium mb-2 text-yellow-800">Payment Method</legend>
+                <div className="flex space-x-4">
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name="payment-method"
+                      value="credit-card"
+                      className="form-radio"
+                      checked={paymentMethod === 'credit-card'}
+                      onChange={handlePaymentMethodChange}
+                    />
+                    <span className="text-lg">Credit Card</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name="payment-method"
+                      value="paypal"
+                      className="form-radio"
+                      checked={paymentMethod === 'paypal'}
+                      onChange={handlePaymentMethodChange}
+                    />
+                    <span className="text-lg">PayPal</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name="payment-method"
+                      value="mpesa"
+                      className="form-radio"
+                      checked={paymentMethod === 'mpesa'}
+                      onChange={handlePaymentMethodChange}
+                    />
+                    <span className="text-lg">M-Pesa</span>
+                  </label>
+                </div>
+              </fieldset>
+              {paymentMethod === 'credit-card' && (
+                <>
+                  <label className="flex flex-col">
+                    <span className="text-lg font-medium mb-2">Card Information</span>
+                    <input
+                      type="text"
+                      className="input-field"
+                      placeholder="Card number"
+                    />
+                  </label>
+                  <label className="flex flex-col">
+                    <span className="text-lg font-medium mb-2">Expiration Date</span>
+                    <input
+                      type="text"
+                      className="input-field"
+                      placeholder="MM/YY"
+                    />
+                  </label>
+                </>
+              )}
+              {paymentMethod === 'mpesa' && (
+                <label className="flex flex-col">
+                  <span className="text-lg font-medium mb-2">M-Pesa Number</span>
+                  <input
+                    type="text"
+                    className="input-field"
+                    placeholder="07xxxxxxxx"
+                  />
+                </label>
+              )}
+            </div>
+            <button
+              type="button"
+              className="donate-button"
+              onClick={handleDonate}
+            >
+              Donate Now
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
